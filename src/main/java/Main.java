16daebf7be17f1c8d5f9e0d5c1bfa6dde6f0b290
GOAT <lucasonyiego283@gmail.com>
2026-02-27 21:18:59 +0300

    handling multiple clients and requests concurrently

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,120 +6,29 @@ import java.net.Socket;
 import java.nio.ByteBuffer;
 
 public class Main {
-  public static void main(String[] args){
+  public static void main(String[] args) throws IOException{
   
     System.err.println("Logs from your program will appear here!");
 
   
   
     ServerSocket serverSocket = null;
-    Socket clientSocket = null;
+
     int port = 9092;
 
-    try {
-      serverSocket = new ServerSocket(port);
-      serverSocket.setReuseAddress(true);
-      clientSocket = serverSocket.accept();
 
-      InputStream in=clientSocket.getInputStream();
-      OutputStream out=clientSocket.getOutputStream();
+    serverSocket = new ServerSocket(port);
+    serverSocket.setReuseAddress(true);
+
 
       while(true){
-
-          //message size is 4 bytes
-          byte[] messageSizeInBytes=new byte[4];
-          if(!readAllDataFromSocket(in,messageSizeInBytes)){
-            break;
-          }
-
-          int messageSize= ByteBuffer.wrap(messageSizeInBytes).getInt();
-
-          //read the rest of the message ,we already have the size to read
-          byte[] messageBytes=new byte[messageSize];
-          if(!readAllDataFromSocket(in,messageBytes)){
-            break;
-          }
-
-
-          ByteBuffer buffer=ByteBuffer.wrap(messageBytes);
-
-          //skip the request api key(2 bytes) and the request api version(2 bytes) , 4 bytes in total
-        /*
-            request api key 2 bytes
-            request api version 2 bytes
-            correlation id 4 bytes
-
-
-         */
-          buffer.getShort();
-          short apiVersion=buffer.getShort();
-
-          short errorCode;
-
-          if(apiVersion<0 || apiVersion>4){
-            errorCode=35;
-          }else{
-            errorCode=0;
-          }
-
-
-          int correlationId=buffer.getInt();
-          System.out.println("Correlation id "+correlationId);
-
-
-            //      00 00 00 13  // message_size:      19 bytes
-            //      ab cd ef 12  // correlation_id:    (matches request)
-                    //start of body.......
-            //      00 00        // error_code:        0 (no error)
-            //      02           // api_keys array length:    1 element
-            //      00 12        // api_key:           18 (ApiVersions)
-            //      00 00        // min_version:       0
-            //      00 04        // max_version:       4
-            //      00           // TAG_BUFFER:        empty
-            //      00 00 00 00  // throttle_time_ms:  0
-            //      00           // TAG_BUFFER:        empty
-                    //end of body
-
-
-          ByteBuffer  body=ByteBuffer.allocate(15);
-
-          body.putShort((short)0);//error code
-          body.put((byte)2);// compact array length
-          body.putShort((short)18);//api key
-          body.putShort((short)0);//min version
-          body.putShort((short)4);//max version
-          body.put((byte)0);//tag buffer
-          body.putInt(0);//throttle time in ms
-          body.put((byte)0);//tag buffer
-
-
-          //sending response
-
-          int responseMessageSize=4+body.capacity(); //correlation id + body
-
-          ByteBuffer response=ByteBuffer.allocate(4+responseMessageSize);
-          response.putInt(responseMessageSize);
-          response.putInt(correlationId);
-          response.put(body.array());
-
-          out.write(response.array());
-          out.flush();
-
+          Socket clientSocket = serverSocket.accept();
+          new Thread(()->{
+              handleClient(clientSocket);
+          }).start();
 
       }
 
-
-    } catch (IOException e) {
-      System.out.println("IOException: " + e.getMessage());
-    } finally {
-      try {
-        if (clientSocket != null) {
-          clientSocket.close();
-        }
-      } catch (IOException e) {
-        System.out.println("IOException: " + e.getMessage());
-      }
-    }
   }
 
 
@@ -136,5 +45,114 @@ public class Main {
     }
     return true;
   }
+
+
+  public static void handleClient(Socket clientSocket){
+
+
+      try {
+
+          InputStream in=clientSocket.getInputStream();
+          OutputStream out=clientSocket.getOutputStream();
+
+          while(true){
+
+              //message size is 4 bytes
+              byte[] messageSizeInBytes=new byte[4];
+              if(!readAllDataFromSocket(in,messageSizeInBytes)){
+                  break;
+              }
+
+              int messageSize= ByteBuffer.wrap(messageSizeInBytes).getInt();
+
+              //read the rest of the message ,we already have the size to read
+              byte[] messageBytes=new byte[messageSize];
+              if(!readAllDataFromSocket(in,messageBytes)){
+                  break;
+              }
+
+
+              ByteBuffer buffer=ByteBuffer.wrap(messageBytes);
+
+              //skip the request api key(2 bytes) and the request api version(2 bytes) , 4 bytes in total
+        /*
+            request api key 2 bytes
+            request api version 2 bytes
+            correlation id 4 bytes
+
+
+         */
+              buffer.getShort();
+              short apiVersion=buffer.getShort();
+
+              short errorCode;
+
+              if(apiVersion<0 || apiVersion>4){
+                  errorCode=35;
+              }else{
+                  errorCode=0;
+              }
+
+
+              int correlationId=buffer.getInt();
+              System.out.println("Correlation id "+correlationId);
+
+
+              //      00 00 00 13  // message_size:      19 bytes
+              //      ab cd ef 12  // correlation_id:    (matches request)
+              //start of body.......
+              //      00 00        // error_code:        0 (no error)
+              //      02           // api_keys array length:    1 element
+              //      00 12        // api_key:           18 (ApiVersions)
+              //      00 00        // min_version:       0
+              //      00 04        // max_version:       4
+              //      00           // TAG_BUFFER:        empty
+              //      00 00 00 00  // throttle_time_ms:  0
+              //      00           // TAG_BUFFER:        empty
+              //end of body
+
+
+              ByteBuffer  body=ByteBuffer.allocate(15);
+
+              body.putShort((short)0);//error code
+              body.put((byte)2);// compact array length
+              body.putShort((short)18);//api key
+              body.putShort((short)0);//min version
+              body.putShort((short)4);//max version
+              body.put((byte)0);//tag buffer
+              body.putInt(0);//throttle time in ms
+              body.put((byte)0);//tag buffer
+
+
+              //sending response
+
+              int responseMessageSize=4+body.capacity(); //correlation id + body
+
+              ByteBuffer response=ByteBuffer.allocate(4+responseMessageSize);
+              response.putInt(responseMessageSize);
+              response.putInt(correlationId);
+              response.put(body.array());
+
+              out.write(response.array());
+              out.flush();
+
+
+          }
+
+
+      } catch (IOException e) {
+          System.out.println("IOException: " + e.getMessage());
+      } finally {
+          try {
+              if (clientSocket != null) {
+                  clientSocket.close();
+              }
+          } catch (IOException e) {
+              System.out.println("IOException: " + e.getMessage());
+          }
+      }
+
+  }
+
 
 }
